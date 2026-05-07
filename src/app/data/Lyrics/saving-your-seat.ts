f1265f02dcbdfbf26fa03d791cf1e4a447ea29a3
Michael Joseph Ojeda <mikelojeda25@gmail.com>
2026-05-07 20:13:21 +0800

    lyrics

---
+++ src/app/data/Lyrics/saving-your-seat.ts
@@ -0,0 +1,51 @@
+export const savingYourSeatLyrics = `Intro
+The sun is hitting the side of my room
+Where we used to talk about
+What our future would be
+
+Verse 1
+Can I still feel your warmth?
+I’m shivering in the cold
+I can’t turn off the alarm
+I’ve been awake since I was told
+That I won’t see you
+
+Just...once...more...
+
+Chorus
+Still saving your seat every dinner
+Brewing a pot of bitter tea you liked
+Staring at your plate, with the food 
+You always loved - going cold
+Did I forget… that you’re gone
+
+Verse 2 
+I turned off the alarm
+same coffee everyday, to ease this weight, 
+My hands are shaking, your face is fading
+My lips refuse to say goodbye
+
+Bridge
+May angels wipe away your tears…
+Carry your soul above the clouds…
+Where no pain lingers Oh..
+
+Oh oh...
+
+Chorus
+Still saving your seat every dinner
+But the tea is cold, and the plate collects dust
+Staring at the wall... memories on repeat
+Did I forget… that you’re gone
+
+Verse 3 
+It’s time... to let go
+I ate dinner alone tonight
+I washed your cup
+
+Oh,find your peace
+Find your peace… 
+Find your peace…
+
+Outro 
+I'll find my peace`;
